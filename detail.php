790cf56041851be7b7eb02b51e5c2bc31866dada
Tomas Juarez <tomasjuarez@gmail.com>
2020-06-11 16:59:11 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -16,7 +16,7 @@ require 'vendor/autoload.php';
 
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
     MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
-
+/*
     $payer = new MercadoPago\Payer();
     $payer->name = "Lalo";
     $payer->surname = "Landa";
@@ -65,7 +65,7 @@ require 'vendor/autoload.php';
     $preference->payer = $payer;
     $preference->external_reference = 'tomasjuarez@gmail.com';
     $preference->save();
-
+*/
     //$link_mp = $preference->init_point;
     $link_pago='';
 
