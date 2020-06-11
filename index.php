d3faaaaead9d203786df6e9a27890c371f1004db
Tomas Juarez <tomasjuarez@gmail.com>
2020-06-11 01:34:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -261,7 +261,7 @@ session_start();
                                                 $15.000
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="detail.php" method="get">
                                             <input type="hidden" name="img" value="./assets/samsung-galaxy-s9-xxl.jpg">
                                             <input type="hidden" name="title" value="Samsung Galaxy S9">
                                             <input type="hidden" name="price" value="15000">
@@ -316,7 +316,7 @@ session_start();
                                                 $10.000
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="detail.php" method="get">
                                             <input type="hidden" name="img" value="./assets/l6g6.jpg">
                                             <input type="hidden" name="title" value="LG G6">
                                             <input type="hidden" name="price" value="10000">
@@ -374,7 +374,7 @@ session_start();
                                                 </span>
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="detail.php" method="get">
                                             <input type="hidden" name="img" value="./assets/u_10168742.jpg">
                                             <input type="hidden" name="title" value="iPhone 8">
                                             <input type="hidden" name="price" value="16000">
@@ -431,7 +431,7 @@ session_start();
                                                 </span>
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="detail.php" method="get">
                                             <input type="hidden" name="img" value="./assets/motorola-moto-g5-plus-1.jpg">
                                             <input type="hidden" name="title" value="Motorola G5">
                                             <input type="hidden" name="price" value="9000">
@@ -481,7 +481,7 @@ session_start();
                                                 $8.000
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="detail.php" method="get">
                                             <input type="hidden" name="img" value="./assets/motorola-moto-g4-3.jpg">
                                             <input type="hidden" name="title" value="Moto G4">
                                             <input type="hidden" name="price" value="8000">
@@ -532,7 +532,7 @@ session_start();
                                                 </span>
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
+                                        <form action="detail.php" method="get">
                                             <input type="hidden" name="img" value="./assets/003.jpg">
                                             <input type="hidden" name="title" value="Sony Xperia XZ2">
                                             <input type="hidden" name="price" value="10000">
