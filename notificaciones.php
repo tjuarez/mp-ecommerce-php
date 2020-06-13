1ed3e91f4d5ab8e5270d20fd2c1a74b911635887
Tomas Juarez <tomasjuarez@gmail.com>
2020-06-13 12:33:29 -0300

    Update notificaciones.php

--- notificaciones.php
+++ notificaciones.php
@@ -37,14 +37,36 @@ http_response_code(200);
     }
 
 */
-    //ini_set( 'display_errors', 1 );
-    //error_reporting( E_ALL );
-    $from = "tomasjuarez@gmail.com";
-    $to = "tomasjuarez@gmail.com";
-    $subject = "MP Ecommerce Notification";
-    $message = "Probando envio de email";
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
+$destinatario = "tomasjuarez@gmail.com"; 
+$asunto = "Este mensaje es de prueba"; 
+$cuerpo = ' 
+<html> 
+<head> 
+   <title>Prueba de correo</title> 
+</head> 
+<body> 
+<h1>Hola amigos!</h1> 
+<p> 
+<b>Bienvenidos a mi correo electrónico de prueba</b>. Estoy encantado de tener tantos lectores. Este cuerpo del mensaje es del artículo de envío de mails por PHP. Habría que cambiarlo para poner tu propio cuerpo. Por cierto, cambia también las cabeceras del mensaje. 
+</p> 
+</body> 
+</html> 
+'; 
+
+//para el envío en formato HTML 
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
+
+//dirección del remitente 
+$headers .= "From: Miguel Angel Alvarez <pepito@desarrolloweb.com>\r\n"; 
+
+//dirección de respuesta, si queremos que sea distinta que la del remitente 
+$headers .= "Reply-To: mariano@desarrolloweb.com\r\n"; 
+
+//ruta del mensaje desde origen a destino 
+$headers .= "Return-path: holahola@desarrolloweb.com\r\n"; 
+
+mail($destinatario,$asunto,$cuerpo,$headers);
 
 echo "FIN";
 
